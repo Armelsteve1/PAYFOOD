cf154f0036873c052c2757fa8d0c45ae7e489b4e
Armel Steve OUETCHOUA <armel.ouetchoua@tilkal.com>
2023-04-18 15:24:16 +0200

    homepage

--- payfood/src/App.js
+++ payfood/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import LandingPage from './Component/HomePage/homePage';
 import NavigationBar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
       <NavigationBar></NavigationBar>
+      <LandingPage></LandingPage>
     </div>
   );
 }
