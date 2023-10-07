f5681da02a763176fa6453724a3b764dfef82c00
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-10-07 16:54:07 +0200

    nav finished

--- payfood/src/Component/Navbar/Navbar.js
+++ payfood/src/Component/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Style.css'; 
+import './nav-style.css'; 
 import logo from  './payfood-logo.svg'
 function NavigationBar() {
   return (
@@ -9,13 +9,13 @@ function NavigationBar() {
       </div>
       <ul className="navbar__list">
         <li className="navbar__item">
-          <a href="#" className="navbar__link" >Blog</a>
+          <a href="#about-us" className="navbar__link" >Blog</a>
         </li>
         <li className="navbar__item">
-          <a href="#" className="navbar__link">Restaurants partenaires</a>
+          <a href="#restaurants-partenaires" className="navbar__link">Restaurants partenaires</a>
         </li>
         <li className="navbar__item">
-          <a href="#" className="navbar__link">Contact</a>
+          <a href="#contact" className="navbar__link contact">Contact</a>
         </li>
       </ul>
     </nav>
