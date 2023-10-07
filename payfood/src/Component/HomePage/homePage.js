68c23dcf0a0edabfb23c440c2ff389fe3152c0d1
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-10-07 18:44:37 +0200

    div side by side

--- payfood/src/Component/HomePage/homePage.js
+++ payfood/src/Component/HomePage/homePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./homePageStyle.css";
-import phone from "./phone.png";
+import logo from "./payfood-logo.svg";
 function LandingPage() {
   // const userCredit = 50; // Crédits de fidélité de l'utilisateur
 
@@ -16,40 +16,38 @@ function LandingPage() {
     <div className="landing-page">
       <main className="contenaire">
         <section className="hero">
-          <div clasName="contenairetext" style={{position: "absolute"}}>
-            <div className="slogant_landing">
+          <div className="content">
+            <div className="slogan-landing">
               Boostez votre chiffre d'affaires grâce à Payfood
             </div>
             <div className="decouvert">
               <p>Découvrez</p>
               <p> Payfood.</p>
             </div>
-            <div class="container">
-              <div class="text1">
+            <div className="text">
+              <p>
                 Chez PayFood, nous accompagnons les restaurateurs les plus
                 talentueux d'île de France. Nous avons créé la plateforme
                 d'engagement ultime pour les restaurants, les hôtels, les bars,
                 les salles de réception et les événements.
-              </div>
-              <div class="text1">
+              </p>
+              <p>
                 Avec PayFood, rejoignez une communauté de milliers de
-                restaurateurs qui servent, stimulent, et connectent leurs
-                clients comme jamais auparavant. Faites partie de cette aventure
-                et réinventez l'expérience culinaire de vos clients grâce à
-                notre plateforme !
-              </div>
+                restaurateurs qui servent, stimulent, et connectent leurs clients
+                comme jamais auparavant. Faites partie de cette aventure et
+                réinventez l'expérience culinaire de vos clients grâce à notre
+                plateforme !
+              </p>
             </div>
             <button className="contact">Contactez-nous</button>
           </div>
-          <div className="circle">
-            {/* <img className="phone" src={phone}></img> */}
-          </div>
+          <img className="phone" src={logo} alt="Phone" />
         </section>
         <section className="deuxieme">
           <div></div>
         </section>
-      </main>
-    </div>
+      </main >
+    </div >
   );
 }
 
