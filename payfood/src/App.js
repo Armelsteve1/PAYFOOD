0deb32ce297aa1a957586b24be15a30f4014616a
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2023-10-16 14:48:50 +0200

    integer google analytics v0

--- payfood/src/App.js
+++ payfood/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import LandingPage from './Component/HomePage/home-page';
 import NavigationBar from './Component/Navbar/navbar';
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "411751010";
+ReactGA.initialize(TRACKING_ID, { debug: true });
+
 function App() {
   return (
     <div className="App">
